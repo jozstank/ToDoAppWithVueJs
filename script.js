3b3bd5c9e06dbd413c425c5398b71f456c69bcad
Tayza Htaine <thoukhabsi@gmail.com>
2023-12-19 20:37:18 +0630

    v2 added filter btns All,Active,Completed

--- script.js
+++ script.js
@@ -4,20 +4,47 @@ const app = createApp({
     return {
       todos: [],
       todoTitle: "",
+      filterStatus: "all",
+      totalTodos: 0,
     };
+  },
+  computed: {
+    filterTodo() {
+      if (this.filterStatus === "all") {
+        return this.todos;
+      }
+      if (this.filterStatus === "active")
+        return this.todos.filter((x) => x.completed === false);
+      if (this.filterStatus === "completed") {
+        return this.todos.filter((x) => x.completed === true);
+      }
+    },
+  },
+  watch: {
+    "filterTodo.length": {
+      handler() {
+        this.totalTodos = this.filterTodo.length;
+      },
+      immediate: true,
+    },
   },
   methods: {
     addTodo() {
-      const newTodo = {
-        id: this.todos.length + 1,
-        title: this.todoTitle,
-        completed: false,
-      };
-      this.todos.push(newTodo);
-      this.todoTitle = "";
+      if (this.todoTitle.length > 0) {
+        const newTodo = {
+          id: this.todos.length + 1,
+          title: this.todoTitle,
+          completed: false,
+        };
+        this.todos.push(newTodo);
+        this.todoTitle = "";
+      }
     },
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    changeFilterStatus(x) {
+      this.filterStatus = x;
     },
   },
 });
